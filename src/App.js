c8355058a9a0f7a82d9278322743c610fbdd0db0
Thomas <thomashaudiquer@gmail.com>
2022-07-16 10:23:58 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(false)
+
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+  }
+
   return (
     <div className="container">
-    <Navbar />
-    <Main />
+    <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+    <Main darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
 </div>
   );
 }
